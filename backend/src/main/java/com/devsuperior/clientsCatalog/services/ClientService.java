b075c573df123fd17f23769eb91ebfc1455345fa
franklyn damaceno <frank-destro@outook.com>
2022-07-05 19:55:39 -0300

    Delete Client

--- backend/src/main/java/com/devsuperior/clientsCatalog/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/clientsCatalog/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.clientsCatalog.dto.ClientDTO;
 import com.devsuperior.clientsCatalog.entities.Client;
 import com.devsuperior.clientsCatalog.repositories.ClientRepository;
+import com.devsuperior.clientsCatalog.services.exceptions.DatabaseException;
 import com.devsuperior.clientsCatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -65,4 +68,13 @@ public class ClientService {
 		}
 	}
 
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id nao encontrado: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de Integridade");
+		}
+	}
 }
