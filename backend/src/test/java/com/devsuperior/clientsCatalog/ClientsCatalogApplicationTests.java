311855e1c0c01997d27f879d429fadf320b42646
franklyn damaceno <frank-destro@outook.com>
2022-07-05 13:48:26 -0300

    client backend project created

---
+++ backend/src/test/java/com/devsuperior/clientsCatalog/ClientsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientsCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
